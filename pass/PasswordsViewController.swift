bd8ba02061f05a3ab76be2277466d126409fbde5
Bob Sun <bob@mssun.me>
2017-02-03 13:01:41 +0800

    refactor search bar

--- pass/PasswordsViewController.swift
+++ pass/PasswordsViewController.swift
@@ -1,8 +1,8 @@
 //
-//  PasswordTableViewController.swift
+//  PasswordsViewController.swift
 //  pass
 //
-//  Created by Mingshen Sun on 18/1/2017.
+//  Created by Mingshen Sun on 3/2/2017.
 //  Copyright © 2017 Bob Sun. All rights reserved.
 //
 
@@ -10,12 +10,14 @@ import UIKit
 import Result
 import SVProgressHUD
 
-
-class PasswordsTableViewController: UITableViewController {
+class PasswordsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     private var passwordEntities: [PasswordEntity]?
     var filteredPasswordEntities = [PasswordEntity]()
-    let searchController = UISearchController(searchResultsController: nil)
     var sections : [(index: Int, length :Int, title: String)] = Array()
+    var searchActive : Bool = false
+    let searchController = UISearchController(searchResultsController: nil)
+
+    @IBOutlet weak var tableView: UITableView!
     
     @IBAction func refreshPasswords(_ sender: UIBarButtonItem) {
         SVProgressHUD.setDefaultMaskType(.black)
@@ -23,7 +25,7 @@ class PasswordsTableViewController: UITableViewController {
         DispatchQueue.global(qos: .userInitiated).async {
             if PasswordStore.shared.pullRepository(transferProgressBlock: {(git_transfer_progress, stop) in
                 DispatchQueue.main.async {
-                SVProgressHUD.showProgress(Float(git_transfer_progress.pointee.received_objects)/Float(git_transfer_progress.pointee.total_objects), status: "Pull Remote Repository")
+                    SVProgressHUD.showProgress(Float(git_transfer_progress.pointee.received_objects)/Float(git_transfer_progress.pointee.total_objects), status: "Pull Remote Repository")
                 }
             }) {
                 DispatchQueue.main.async {
@@ -42,16 +44,49 @@ class PasswordsTableViewController: UITableViewController {
         super.viewDidLoad()
         passwordEntities = PasswordStore.shared.fetchPasswordEntityCoreData()
         NotificationCenter.default.addObserver(self, selector: #selector(PasswordsTableViewController.actOnPasswordUpdatedNotification), name: NSNotification.Name(rawValue: "passwordUpdated"), object: nil)
+        generateSections(item: passwordEntities!)
+        tableView.delegate = self
+        tableView.dataSource = self
         searchController.searchResultsUpdater = self
         searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.sizeToFit()
         definesPresentationContext = true
-        // tableView.setContentOffset(CGPoint(x: 0, y: 44), animated: false)
-        generateSections(item: passwordEntities!)
-        tableView.tableHeaderView = searchController.searchBar
+        let searchBarView = UIView(frame: CGRect(x: 0, y: 64, width: searchController.searchBar.frame.size.width, height: 44))
+        searchBarView.addSubview(searchController.searchBar)
+        view.addSubview(searchBarView)
     }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
+     func numberOfSections(in tableView: UITableView) -> Int {
         return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].length
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "passwordTableViewCell", for: indexPath)
+        var password: PasswordEntity
+        let index = sections[indexPath.section].index + indexPath.row
+        if searchController.isActive && searchController.searchBar.text != "" {
+            password = filteredPasswordEntities[index]
+        } else {
+            password = passwordEntities![index]
+        }
+        cell.textLabel?.text = password.name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].title
+    }
+
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return sections.map { $0.title }
+    }
+
+    func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        return index
     }
     
     func generateSections(item: [PasswordEntity]) {
@@ -77,6 +112,30 @@ class PasswordsTableViewController: UITableViewController {
         sections.append(newSection)
     }
     
+    func actOnPasswordUpdatedNotification() {
+        passwordEntities = PasswordStore.shared.fetchPasswordEntityCoreData()
+        generateSections(item: passwordEntities!)
+        self.tableView.reloadData()
+        print("actOnPasswordUpdatedNotification")
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showPasswordDetail" {
+            if let viewController = segue.destination as? PasswordDetailTableViewController {
+                let selectedIndexPath = self.tableView.indexPath(for: sender as! UITableViewCell)!
+                let index = sections[selectedIndexPath.section].index + selectedIndexPath.row
+                let password: PasswordEntity
+                if searchController.isActive && searchController.searchBar.text != "" {
+
+                    password = filteredPasswordEntities[index]
+                } else {
+                    password = passwordEntities![index]
+                }
+                viewController.passwordEntity = password
+                viewController.navigationItem.title = password.name
+            }
+        }
+    }
     func filterContentForSearchText(searchText: String, scope: String = "All") {
         filteredPasswordEntities = passwordEntities!.filter { password in
             return password.name!.lowercased().contains(searchText.lowercased())
@@ -88,72 +147,10 @@ class PasswordsTableViewController: UITableViewController {
         }
         tableView.reloadData()
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-    }
-    
-    func actOnPasswordUpdatedNotification() {
-        passwordEntities = PasswordStore.shared.fetchPasswordEntityCoreData()
-        self.tableView.reloadData()
-        print("actOnPasswordUpdatedNotification")
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        if searchController.isActive && searchController.searchBar.text != "" {
-//            return filteredPasswordEntities.count
-//        }
-//        return passwordEntities!.count
-        return sections[section].length
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "passwordTableViewCell", for: indexPath)
-        var password: PasswordEntity
-        let index = sections[indexPath.section].index + indexPath.row
-        if searchController.isActive && searchController.searchBar.text != "" {
-            password = filteredPasswordEntities[index]
-        } else {
-            password = passwordEntities![index]
-        }
-        cell.textLabel?.text = password.name
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sections[section].title
-    }
-    
-    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-        return sections.map { $0.title }
-    }
-    
-    override func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
-        return index
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showPasswordDetail" {
-            if let viewController = segue.destination as? PasswordDetailTableViewController {
-                let selectedIndexPath = self.tableView.indexPath(for: sender as! UITableViewCell)!
-                let index = sections[selectedIndexPath.section].index + selectedIndexPath.row
-                let password: PasswordEntity
-                if searchController.isActive && searchController.searchBar.text != "" {
-                    password = filteredPasswordEntities[index]
-                } else {
-                    password = passwordEntities![index]
-                }
-                viewController.passwordEntity = password
-                viewController.navigationItem.title = password.name
-            }
-        }
-    }
-    
 }
 
-extension PasswordsTableViewController: UISearchResultsUpdating {
+extension PasswordsViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
         filterContentForSearchText(searchText: searchController.searchBar.text!)
     }
 }
-
