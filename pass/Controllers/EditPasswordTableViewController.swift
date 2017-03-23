2107954cd39a4689e610dd6df63b4350932e8cff
Yishi Lin <yishilin14@gmail.com>
2017-03-24 01:10:26 +0800

    Fix a bug introduced by commit 5a02cb7 .
    - In that commit, the "delete password" in the edit view would be disabled by the presence of the "password length slider".

--- pass/Controllers/EditPasswordTableViewController.swift
+++ pass/Controllers/EditPasswordTableViewController.swift
@@ -12,7 +12,8 @@ class EditPasswordTableViewController: PasswordEditorTableViewController {
     override func viewDidLoad() {
         tableData = [
             [[.type: PasswordEditorCellType.textFieldCell, .title: "name", .content: password!.name]],
-            [[.type: PasswordEditorCellType.fillPasswordCell, .title: "password", .content: password!.password]],
+            [[.type: PasswordEditorCellType.fillPasswordCell, .title: "password", .content: password!.password],
+             [.type: PasswordEditorCellType.passwordLengthCell, .title: "passwordlength"]],
             [[.type: PasswordEditorCellType.textViewCell, .title: "additions", .content: password!.getAdditionsPlainText()]],
             [[.type: PasswordEditorCellType.deletePasswordCell]],
         ]
