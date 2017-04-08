bef9e0251ccd7a686b43838302e510c472465e2d
Yishi Lin <yishilin14@gmail.com>
2017-04-08 23:51:43 +0800

    Ask to save passphrase while setting PGP keys through URLs

--- pass/Controllers/PGPKeyArmorSettingTableViewController.swift
+++ pass/Controllers/PGPKeyArmorSettingTableViewController.swift
@@ -22,12 +22,9 @@ class PGPKeyArmorSettingTableViewController: UITableViewController, UITextViewDe
         armorPublicKeyTextView.text = Defaults[.pgpPublicKeyArmor]
         armorPrivateKeyTextView.text = Defaults[.pgpPrivateKeyArmor]
         pgpPassphrase = passwordStore.pgpKeyPassphrase
-        
-        armorPublicKeyTextView.delegate = self
-        armorPrivateKeyTextView.delegate = self
     }
     
-    private func createSavePassphraseAlert() -> UIAlertController {
+    private func createSavePassphraseAndSegueAlert() -> UIAlertController {
         let savePassphraseAlert = UIAlertController(title: "Passphrase", message: "Do you want to save the passphrase for later decryption?", preferredStyle: UIAlertControllerStyle.alert)
         savePassphraseAlert.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.default) { _ in
             Defaults[.isRememberPassphraseOn] = false
@@ -44,8 +41,8 @@ class PGPKeyArmorSettingTableViewController: UITableViewController, UITextViewDe
         let alert = UIAlertController(title: "Passphrase", message: "Please fill in the passphrase of your PGP secret key.", preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: {_ in
             self.pgpPassphrase = alert.textFields?.first?.text
-            let savePassphraseAlert = self.createSavePassphraseAlert()
-            self.present(savePassphraseAlert, animated: true, completion: nil)
+            let savePassphraseAndSegueAlert = self.createSavePassphraseAndSegueAlert()
+            self.present(savePassphraseAndSegueAlert, animated: true, completion: nil)
         }))
         alert.addTextField(configurationHandler: {(textField: UITextField!) in
             textField.text = self.pgpPassphrase
@@ -56,7 +53,7 @@ class PGPKeyArmorSettingTableViewController: UITableViewController, UITextViewDe
     
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         if text == UIPasteboard.general.string {
-            // user pastes somethint, get ready to clear in 10s
+            // user pastes something, get ready to clear in 10s
             recentPastedText = text
             DispatchQueue.global(qos: .background).asyncAfter(deadline: DispatchTime.now() + 10) { [weak weakSelf = self] in
                 if let pasteboardString = UIPasteboard.general.string,
