b48a7438ed4978b3fb74f26591cb50034633a70a
Bob Sun <bob@mssun.me>
2017-06-03 22:45:53 -0700

    Avoid crash when table is empty

--- pass/Controllers/PasswordsViewController.swift
+++ pass/Controllers/PasswordsViewController.swift
@@ -535,7 +535,9 @@ class PasswordsViewController: UIViewController, UITableViewDataSource, UITableV
             self.tapTabBarTime = currentTime
             if duration < 0.35 {
                 let topIndexPath = IndexPath(row: 0, section: 0)
-                tableView.scrollToRow(at: topIndexPath, at: .bottom, animated: true)
+                if let _ = tableView.cellForRow(at: topIndexPath) {
+                    tableView.scrollToRow(at: topIndexPath, at: .bottom, animated: true)
+                }
                 self.tapTabBarTime = 0
                 return
             }
