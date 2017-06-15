a8060d28c9d3b2eb7928266f29eef0c4a86b04ca
Yishi Lin <yishilin14@gmail.com>
2017-06-15 17:30:57 +0800

    Fix some startup warnings

--- passKit/Models/PasswordStore.swift
+++ passKit/Models/PasswordStore.swift
@@ -74,6 +74,9 @@ public class PasswordStore {
         let modelURL = Bundle(identifier: Globals.passKitBundleIdentifier)!.url(forResource: "pass", withExtension: "momd")!
         let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL)
         let container = NSPersistentContainer(name: "pass", managedObjectModel: managedObjectModel!)
+        if FileManager.default.fileExists(atPath: Globals.documentPath) {
+            try! FileManager.default.createDirectory(atPath: Globals.documentPath, withIntermediateDirectories: true, attributes: nil)
+        }
         container.persistentStoreDescriptions = [NSPersistentStoreDescription(url: URL(fileURLWithPath: Globals.dbPath))]
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
