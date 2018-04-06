3ffbf68ab860cda74113b183813711b9294be406
Yishi Lin <yishilin14@gmail.com>
2018-04-07 00:17:00 +0800

    Avoid crash during yaml parsing

--- passKit/Models/Password.swift
+++ passKit/Models/Password.swift
@@ -149,7 +149,9 @@ public class Password {
         if checkDuplicatedFields(lines: fromYaml) {
             throw AppError.YamlLoadError
         }
-        let yamlFile = try Yams.load(yaml: fromYaml) as! [String: Any]
+        guard let yamlFile = try Yams.load(yaml: fromYaml) as? [String: Any] else {
+            throw AppError.YamlLoadError
+        }
         additions.append(contentsOf: yamlFile.map { ($0, String(describing: $1)) })
     }
 
