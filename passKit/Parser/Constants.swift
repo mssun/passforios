1f57305203b0e2b4e6121cb360a015b94d9723f7
Danny Moesch <danny.moesch@icloud.com>
2018-11-14 21:29:25 +0100

    Use constants for otp_* and other related strings

--- passKit/Parser/Constants.swift
+++ passKit/Parser/Constants.swift
@@ -8,15 +8,31 @@
 
 public struct Constants {
 
+    static let OTP_SECRET = "otp_secret"
+    static let OTP_TYPE = "otp_type"
+    static let OTP_ALGORITHM = "otp_algorithm"
+    static let OTP_PERIOD = "otp_period"
+    static let OTP_DIGITS = "otp_digits"
+    static let OTP_COUNTER = "otp_counter"
+    static let OTPAUTH = "otpauth"
+
     public static let OTP_KEYWORDS = [
-        "otp_secret",
-        "otp_type",
-        "otp_algorithm",
-        "otp_period",
-        "otp_digits",
-        "otp_counter",
-        "otpauth",
+        OTP_SECRET,
+        OTP_TYPE,
+        OTP_ALGORITHM,
+        OTP_PERIOD,
+        OTP_DIGITS,
+        OTP_COUNTER,
+        OTPAUTH,
     ]
+
+    static let TOTP = "totp"
+    static let HOTP = "hotp"
+    static let SHA256 = "sha256"
+    static let SHA512 = "sha512"
+    static let DEFAULT_DIGITS = "6"
+    static let DEFAULT_PERIOD = "30.0"
+    static let DEFAULT_COUNTER = ""
 
     static let BLANK = " "
     static let MULTILINE_WITH_LINE_BREAK_INDICATOR = "|"
@@ -24,7 +40,6 @@ public struct Constants {
     static let MULTILINE_WITHOUT_LINE_BREAK_INDICATOR = ">"
     static let MULTILINE_WITHOUT_LINE_BREAK_SEPARATOR = BLANK
 
-    static let OTPAUTH = "otpauth"
     static let OTPAUTH_URL_START = "\(OTPAUTH)://"
     static let PASSWORD_KEYWORD = "password"
     static let USERNAME_KEYWORD = "username"
