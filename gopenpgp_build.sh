083c37f4e79a18944231a254cb090b93b9926080
Moritz Kuntze <mfk@zortac.net>
2019-06-06 18:07:39 +0200

    Automated build of GopenPGP

---
+++ gopenpgp_build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+OLDGOPATH=$GOPATH
+
+export GOPATH=$(pwd)/go
+
+go get -u golang.org/x/mobile/cmd/gomobile
+go install golang.org/x/mobile/cmd/gobind
+go get golang.org/x/mobile
+
+go get -u github.com/ProtonMail/gopenpgp
+
+cd $GOPATH/src/github.com/ProtonMail/gopenpgp
+
+git fetch && git fetch --tags
+
+git checkout v0
+
+GO111MODULE=on go mod vendor
+
+git checkout v1.0.0
+
+cd $GOPATH
+mkdir dist
+
+$GOPATH/bin/gomobile bind -target ios -o dist/Gopenpgpwrapper.framework gopenpgpwrapper
+
+export GOPATH=$OLDGOPATH
