5f19c348c8fd2ec8d8c95d3d5527268192b77ee8
Danny Moesch <danny.moesch@icloud.com>
2019-07-02 19:34:12 +0200

    Keep global constants for armor keys and clean them up during migration and user initiated key removal

--- passKit/Helpers/DefaultsKeys.swift
+++ passKit/Helpers/DefaultsKeys.swift
@@ -16,6 +16,10 @@ public extension DefaultsKeys {
     static let pgpPublicKeyURL = DefaultsKey<URL?>("pgpPublicKeyURL")
     static let pgpPrivateKeyURL = DefaultsKey<URL?>("pgpPrivateKeyURL")
 
+    // Keep them for legacy reasons.
+    static let pgpPublicKeyArmor = DefaultsKey<String?>("pgpPublicKeyArmor")
+    static let pgpPrivateKeyArmor = DefaultsKey<String?>("pgpPrivateKeyArmor")
+
     static let gitURL = DefaultsKey<URL?>("gitURL")
     static let gitAuthenticationMethod = DefaultsKey<String?>("gitAuthenticationMethod")
     static let gitUsername = DefaultsKey<String?>("gitUsername")
