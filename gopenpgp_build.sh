f4f038375b018d5a03922453d8bf37bbbafade87
Mingshen Sun <bob@mssun.me>
2020-04-12 18:24:03 -0700

    Support gnu-dummy

--- gopenpgp_build.sh
+++ gopenpgp_build.sh
@@ -2,20 +2,23 @@
 
 set -euox pipefail
 
+mkdir -p go
 export GOPATH="$(pwd)/go"
 export PATH="$PATH:$GOPATH/bin"
 
 go get -u golang.org/x/mobile/cmd/gomobile || true
 gomobile init
-go get -u github.com/ProtonMail/gopenpgp || true
+go get -u github.com/mssun/gopenpgp || true
 
-PACKAGE_PATH="github.com/ProtonMail/gopenpgp"
-GOPENPGP_REVISION="v2.0.0"
+PACKAGE_PATH="github.com/mssun/gopenpgp"
+mkdir -p $GOPATH/src/github.com/ProtonMail
+GOPENPGP_REVISION="gnu-dummy"
+ln -s $GOPATH/src/github.com/mssun/gopenpgp $GOPATH/src/github.com/ProtonMail/gopenpgp
 
 ( cd "$GOPATH/src/$PACKAGE_PATH" && git checkout "$GOPENPGP_REVISION" && GO111MODULE=on go mod vendor )
-#patch -p0 < $GOPATH/crypto.patch
 
 OUTPUT_PATH="$GOPATH/dist"
 mkdir -p "$OUTPUT_PATH"
+
 "$GOPATH/bin/gomobile" bind -v -ldflags="-s -w" -target ios -o "${OUTPUT_PATH}/Crypto.framework" \
     "$PACKAGE_PATH"/{crypto,armor,constants,models,subtle}
