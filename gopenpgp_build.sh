78d6e6013361cc762a98271b54fbf88301240140
Danny Moesch <danny.moesch@icloud.com>
2020-04-19 15:44:09 +0200

    Introduce variable for GopenPGP repository

--- gopenpgp_build.sh
+++ gopenpgp_build.sh
@@ -2,22 +2,24 @@
 
 set -euox pipefail
 
-mkdir -p go
 export GOPATH="$(pwd)/go"
 export PATH="$PATH:$GOPATH/bin"
 
+PACKAGE_PATH="github.com/mssun/gopenpgp"
+GOPENPGP_REVISION="gnu-dummy"
+OUTPUT_PATH="$GOPATH/dist"
+
+mkdir -p "$GOPATH"
+
 go get -u golang.org/x/mobile/cmd/gomobile || true
 gomobile init
-go get -u github.com/mssun/gopenpgp || true
+go get -u "$PACKAGE_PATH" || true
 
-PACKAGE_PATH="github.com/mssun/gopenpgp"
-mkdir -p $GOPATH/src/github.com/ProtonMail
-GOPENPGP_REVISION="gnu-dummy"
-ln -s $GOPATH/src/github.com/mssun/gopenpgp $GOPATH/src/github.com/ProtonMail/gopenpgp
+mkdir -p "$GOPATH/src/github.com/ProtonMail"
+ln -s "$GOPATH/src/$PACKAGE_PATH" "$GOPATH/src/github.com/ProtonMail/gopenpgp"
 
 ( cd "$GOPATH/src/$PACKAGE_PATH" && git checkout "$GOPENPGP_REVISION" && GO111MODULE=on go mod vendor )
 
-OUTPUT_PATH="$GOPATH/dist"
 mkdir -p "$OUTPUT_PATH"
 
 "$GOPATH/bin/gomobile" bind -v -ldflags="-s -w" -target ios -o "${OUTPUT_PATH}/Crypto.framework" \
