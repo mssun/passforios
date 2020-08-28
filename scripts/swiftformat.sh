865de759ad4fbc6160e62a4c5aac8aa78d2bb2f8
Danny Moesch <danny.moesch@icloud.com>
2020-08-28 18:29:15 +0200

    Skip SwiftLint and SwiftFormat runs on CI build
    Unfortunately, there seems to be no simpler way to tell a script in Xcode that it runs on CI.

---
+++ scripts/swiftformat.sh
@@ -0,0 +1,24 @@
+SWIFTFORMAT_VERSION="0.45.*"
+
+if [[ -f "${SRCROOT}/.ci-env" ]]; then
+  echo "Running in a Continuous Integration environment. Formatting is skipped."
+  return
+fi
+
+if [[ "${CONFIGURATION}" != "Debug" ]]; then
+  echo "Running during a release build. Formatting is skipped."
+  return
+fi
+
+if which swiftformat > /dev/null; then
+  if [[ "$(swiftformat --version)" == $SWIFTFORMAT_VERSION ]]; then
+    swiftformat .
+  else
+    echo "Failure: SwiftFormat $SWIFTFORMAT_VERSION is required. Install it or update the build script to use a newer version."
+    exit 1
+  fi
+else
+  echo "Failure: SwiftFormat not installed. Get it via 'brew install swiftformat'."
+  exit 2
+fi
+
