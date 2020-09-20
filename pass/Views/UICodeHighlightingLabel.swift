4ffbafc47e01deb988c4c4598d5229ef40cbc1c7
Danny Moesch <danny.moesch@icloud.com>
2020-09-20 13:46:54 +0200

    Fix file header format

--- pass/Views/UICodeHighlightingLabel.swift
+++ pass/Views/UICodeHighlightingLabel.swift
@@ -1,10 +1,10 @@
-/*
- UICodeHighlightingLabel.swift
- pass
-
- Created by Danny Moesch on 20.01.19.
- Copyright © 2019 Bob Sun. All rights reserved.
- */
+//
+//  UICodeHighlightingLabel.swift
+//  pass
+//
+//  Created by Danny Moesch on 20.01.19.
+//  Copyright © 2019 Bob Sun. All rights reserved.
+//
 
 import passKit
 import UIKit
