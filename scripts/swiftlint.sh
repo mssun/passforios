e2ba21587c37fd0bc354ede3f21254347995d0ec
Danny Mösch <danny.moesch@icloud.com>
2022-11-01 19:47:26 +0100

    Update SwiftLint to version 0.49.x (#576)

--- scripts/swiftlint.sh
+++ scripts/swiftlint.sh
@@ -1,6 +1,6 @@
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}"
 
-SWIFTLINT_VERSION="0.47.*"
+SWIFTLINT_VERSION="0.49.*"
 
 if [[  "${CI}" == "true" ]]; then
   echo "Running in a Continuous Integration environment. Linting is skipped."
